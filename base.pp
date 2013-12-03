167a85e2a969941edbb10c96b64bbd82edfb3540
root <root@chaoshost.chaos>
2013-12-03 20:00:05 +0100

    Initial commit

---
+++ base.pp
@@ -0,0 +1,9 @@
+class profile::base ($basepackages = any2array(hiera_array("basepackages")),
+                     $unwantedpackages = any2array(hiera_array("unwantedpackages")),  
+                     ) {
+
+  package { $basepackages: ensure => present }
+  package { $unwantedpackages: ensure => absent }
+
+  include etckeeper
+}
