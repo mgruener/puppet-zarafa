647c9a25c8a96eccef4f62cba9d80c8bc4eb5a18
root <root@chaoshost.chaos>
2013-12-05 21:14:26 +0100

    extend base profile with usermanagement, basic sysctl configuration and the removal of the dreaded command aliases for rm,cp and mv for the root account

--- base.pp
+++ base.pp
@@ -1,10 +1,31 @@
-class profile::base ($basepackages = any2array(hiera_array("basepackages")),
-                     $unwantedpackages = any2array(hiera_array("unwantedpackages")),  
-                     ) {
+class profile::base ($basepackages = any2array(hiera_array('basepackages')),
+                     $unwantedpackages = any2array(hiera_array('unwantedpackages')),
+                     $sysctlvalues = hiera_hash('sysctlvalues')) {
 
   package { $basepackages: ensure => present }
   package { $unwantedpackages: ensure => absent }
 
   include etckeeper
   include network
+  include usermanagement
+
+  file_line { "root unalias cp":
+    ensure => absent,
+    line => "alias cp='cp -i'",
+    path => "/root/.bashrc"
+  }
+
+  file_line { "root unalias mv":
+    ensure => absent,
+    line => "alias mv='mv -i'",
+    path => "/root/.bashrc"
+  }
+
+  file_line { "root unalias rm":
+    ensure => absent,
+    line => "alias rm='rm -i'",
+    path => "/root/.bashrc"
+  }
+
+  create_resources('sysctl',$sysctlvalues)
 }
