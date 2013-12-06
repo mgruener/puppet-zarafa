5b8ad19da0d75b1d2de3a73de4f288c6a3e0c150
root <root@chaoshost.chaos>
2013-12-06 19:26:24 +0100

    Manage sshd configuration,sshd subsystems and grub kernel parameter Only try to create these resources when the necessary parameter has been provided

--- base.pp
+++ base.pp
@@ -1,6 +1,9 @@
 class profile::base ($basepackages = any2array(hiera_array('basepackages')),
                      $unwantedpackages = any2array(hiera_array('unwantedpackages')),
-                     $sysctlvalues = hiera_hash('sysctlvalues')) {
+                     $sysctlvalues = hiera_hash('sysctlvalues',undef),
+                     $grubkernelparams = hiera_hash('grubkernelparameter',undef),
+                     $sshd_config = hiera_hash('sshd_config',undef),
+                     $sshd_subsystems = hiera_hash('sshd_subsystems',undef)) {
 
   package { $basepackages: ensure => present }
   package { $unwantedpackages: ensure => absent }
@@ -27,5 +30,16 @@ class profile::base ($basepackages = any2array(hiera_array('basepackages')),
     path => "/root/.bashrc"
   }
 
-  create_resources('sysctl',$sysctlvalues)
+  if $sysctlvalues != undef {
+    create_resources('sysctl',$sysctlvalues)
+  }
+  if $grubkernelparams != undef {
+    create_resources('kernel_parameter',$grubkernelparams)
+  }
+  if $sshd_config != undef {
+    create_resources('sshd_config',$sshd_config)
+  }
+  if $sshd_subsystems != undef {
+    create_resources('sshd_config_subsystem',$sshd_subsystems)
+  }
 }
