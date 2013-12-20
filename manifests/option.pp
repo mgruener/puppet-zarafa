e9775870b4d5d122d14a5bd44aa9db4f7618212b
Michael Gruener <michael.gruener@chaosmoon.net>
2013-12-20 18:11:10 +0100

    Add parameter "option" to specify the name of the option (defaults to $title) instead of always using $title as the option name. This prevents a problem where the same option has to be set for different zarafa components.

--- manifests/option.pp
+++ manifests/option.pp
@@ -1,9 +1,9 @@
-define zarafa::option ( $file, $value ) {
+define zarafa::option ( $file, $option = $title, $value ) {
   augeas { "zarafa-${file}-${title}":
     context => "/files${file}",
     incl    => $file,
     lens    => 'Zarafa.lns',
-    changes => "set ${title} '${value}'",
+    changes => "set ${option} '${value}'",
     require => File["${zarafa::augeas_lensdir}/zarafa.aug"]
   }
 }
