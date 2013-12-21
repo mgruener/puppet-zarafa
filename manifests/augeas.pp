2b6b4b8023cf17d47dfe3654a1b26cb272ccb84b
Michael Gruener <michael.gruener@chaosmoon.net>
2013-12-22 00:25:00 +0100

    Move the installation of the augeas lens to its own class so it can be included where necessary (especially in the component manifests)

---
+++ manifests/augeas.pp
@@ -0,0 +1,11 @@
+class zarafa::augeas (
+  $lensdir = hiera("${module_name}::augeas::lensdir",'/usr/share/augeas/lenses/dist')
+) {
+  file { "${lensdir}/zarafa.aug":
+    ensure => file,
+    source => "puppet:///modules/${module_name}/zarafa.aug",
+    owner  => root,
+    group  => root,
+    mode   => '0644'
+  }
+}
