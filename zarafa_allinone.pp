1193930649e4f63c4f54c47fe5af130c1c42e576
Michael Gruener <michael.gruener@chaosmoon.net>
2013-12-21 17:45:00 +0100

    Create certificates for all components

--- zarafa_allinone.pp
+++ zarafa_allinone.pp
@@ -24,7 +24,14 @@ class profile::zarafa_allinone (
   Class['zarafa::server'] -> Class['zarafa::monitor']
   Class['zarafa::server'] -> Class['zarafa::search']
 
+  $certdir   = "/etc/zarafa/ssl"
+  $keydir    = "/etc/pki/tls/private"
+  $pubkeydir = "/etc/zarafa/sslkeys"
+
   Certtool::Cert {
+    certpath        => $certdir,
+    keypath         => $keydir,
+    pubkeypath      => $pubkeydir,
     organization    => $certdata[organization],
     unit            => $certdata[unit],
     locality        => $certdata[locality],
@@ -34,8 +41,13 @@ class profile::zarafa_allinone (
     expiration_days => $certdata[expidation_days]
   }
   
+  file { [ $certdir, $keydir, $pubkeydir ]:
+    ensure => directory,
+  }
+
   certtool::cert { $certdata[caname]:
-    is_ca => true
+    is_ca => true,
+    require => [ File[$certdir], File[$keydir] ]
   }
 
   $usage = [ "tls_www_server",
@@ -53,24 +65,89 @@ class profile::zarafa_allinone (
   certtool::cert { $serverhostname:
     usage => $usage,
     unit => "test",
-    notify => Exec[join-zarafa-ssl-keyfile]
+    extract_pubkey => true,
+    notify => Exec[join-zarafa-ssl-keyfile],
+    require => [ File[$certdir], File[$keydir] ]
   }
 
-  certtool::cert { "${serverhostname}-spooler":
-    usage => $usage
+  certtool::cert { "${serverhostname}-dagent":
+    usage => $usage,
     unit => "test",
-    notify => Exec[join-spooler-ssl-keyfile]
+    extract_pubkey => true,
+    notify => Exec[join-dagent-ssl-keyfile],
+    require => [ File[$certdir], File[$keydir] ]
+  }
+  certtool::cert { "${serverhostname}-ical":
+    usage => $usage,
+    unit => "test",
+    extract_pubkey => true,
+    notify => Exec[join-ical-ssl-keyfile],
+    require => [ File[$certdir], File[$keydir] ]
+  }
+  certtool::cert { "${serverhostname}-gateway":
+    usage => $usage,
+    unit => "test",
+    extract_pubkey => true,
+    notify => Exec[join-gateway-ssl-keyfile],
+    require => [ File[$certdir], File[$keydir] ]
+  }
+  certtool::cert { "${serverhostname}-search":
+    usage => $usage,
+    unit => "test",
+    extract_pubkey => true,
+    notify => Exec[join-search-ssl-keyfile],
+    require => [ File[$certdir], File[$keydir] ]
+  }
+  certtool::cert { "${serverhostname}-monitor":
+    usage => $usage,
+    unit => "test",
+    extract_pubkey => true,
+    notify => Exec[join-monitor-ssl-keyfile],
+    require => [ File[$certdir], File[$keydir] ]
   }
 
   exec { 'join-zarafa-ssl-keyfile':
     path => "/bin:/usr/bin:/sbin:/usr/sbin",
-    command => "cat /etc/pki/tls/private/${serverhostname}.key >> /etc/pki/tls/certs/${serverhostname}.crt",
-    refreshonly => true
+    command => "cat ${keydir}/${serverhostname}.key >> ${certdir}/${serverhostname}.crt",
+    refreshonly => true,
+    require => Certtool::Cert[$serverhostname]
   }
 
   exec { 'join-spooler-ssl-keyfile':
     path => "/bin:/usr/bin:/sbin:/usr/sbin",
-    command => "cat /etc/pki/tls/private/${serverhostname}-spooler.key >> /etc/pki/tls/certs/${serverhostname}-spooler.crt",
-    refreshonly => true
+    command => "cat ${keydir}/${serverhostname}-spooler.key >> ${certdir}/${serverhostname}-spooler.crt",
+    refreshonly => true,
+    require => Certtool::Cert["${serverhostname}-spooler"]
   }
+  exec { 'join-dagent-ssl-keyfile':
+    path => "/bin:/usr/bin:/sbin:/usr/sbin",
+    command => "cat ${keydir}/${serverhostname}-dagent.key >> ${certdir}/${serverhostname}-dagent.crt",
+    refreshonly => true,
+    require => Certtool::Cert["${serverhostname}-dagent"]
+  }
+  exec { 'join-ical-ssl-keyfile':
+    path => "/bin:/usr/bin:/sbin:/usr/sbin",
+    command => "cat ${keydir}/${serverhostname}-ical.key >> ${certdir}/${serverhostname}-ical.crt",
+    refreshonly => true,
+    require => Certtool::Cert["${serverhostname}-ical"]
+  }
+  exec { 'join-gateway-ssl-keyfile':
+    path => "/bin:/usr/bin:/sbin:/usr/sbin",
+    command => "cat ${keydir}/${serverhostname}-gateway.key >> ${certdir}/${serverhostname}-gateway.crt",
+    refreshonly => true,
+    require => Certtool::Cert["${serverhostname}-gateway"]
+  }
+  exec { 'join-monitor-ssl-keyfile':
+    path => "/bin:/usr/bin:/sbin:/usr/sbin",
+    command => "cat ${keydir}/${serverhostname}-monitor.key >> ${certdir}/${serverhostname}-monitor.crt",
+    refreshonly => true,
+    require => Certtool::Cert["${serverhostname}-monitor"]
+  }
+  exec { 'join-search-ssl-keyfile':
+    path => "/bin:/usr/bin:/sbin:/usr/sbin",
+    command => "cat ${keydir}/${serverhostname}-search.key >> ${certdir}/${serverhostname}-search.crt",
+    refreshonly => true,
+    require => Certtool::Cert["${serverhostname}-search"]
+  }
+
 }
