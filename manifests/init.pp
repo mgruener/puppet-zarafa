30fd2d56349e4b60db79ba928365e8d5814e556a
Michael Gruener <michael.gruener@chaosmoon.net>
2013-12-22 01:36:17 +0100

    Remove the duplicate definition for the zarafa augeas lens file resource from the base class. It should only be defined in the augeas class.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,3 @@
 class zarafa {
-  $augeas_lensdir = "/usr/share/augeas/lenses/dist"
-
-  file { "${augeas_lensdir}/zarafa.aug":
-    ensure => file,
-    source => "puppet:///modules/${module_name}/zarafa.aug",
-    owner  => root,
-    group  => root,
-    mode   => '0644'
-  }
-
   include zarafa::allinone
 }
