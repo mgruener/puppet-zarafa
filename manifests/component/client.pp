65375c6de2cfe00d85b6293ac8dd1a2a574eb505
Michael Gruener <michael.gruener@chaosmoon.net>
2013-12-22 01:13:35 +0100

    Add the configfile, options, sslkeyfile and serverhostname parameters and logic to the client class so that even the client component can be used on a different host than the one the server component is running on.
    Add the serverhostname parameter and logic to the webaccess class in
    preparation for whatever is necessary to make the webaccess work.

--- manifests/component/client.pp
+++ manifests/component/client.pp
@@ -1,7 +1,31 @@
 class zarafa::component::client (
+  $serverhostname = hiera("${module_name}::component::server::hostname",'localhost'),
   $packages = hiera("${module_name}::component::client::packages",['zarafa-client', 'zarafa-utils']),
+  $sslkeyfile     = hiera("${module_name}::component::client::sslkeyfile","/etc/zarafa/ssl/${::fqdn}-admin.crt"),
+  $options        = hiera_hash("${module_name}::component::client::options",{}),
+  $configfile     = hiera("${module_name}::component::client::configfile",'/etc/zarafa/admin.cfg')
 ) {
   package { $packages:
     ensure => present
   }
+
+  if downcase($serverhostname) in downcase([ $::fqdn, $::hostname ]) {
+    $zarafaserver = 'localhost'
+  }
+  else {
+    $zarafaserver = $serverhostname
+  }
+
+  $client_options = { 'server_socket-client' => { option => 'server_socket',
+                                                  value => "https://${zarafaserver}:237/zarafa"
+                      },
+                      'sslkey_file-client'    => { option => 'sslkey_file',
+                                                   value  => $sslkeyfile
+                      }
+  }
+
+  create_resources('zarafa::option',merge($client_options,$options), { file    => $configfile, 
+                                                                       require => Package[$packages],
+                                                                       notify  => Service['zarafa-dagent']
+                                                                     })
 }
--- manifests/component/webaccess.pp
+++ manifests/component/webaccess.pp
@@ -1,7 +1,16 @@
 class zarafa::component::webaccess (
+  $serverhostname = hiera("${module_name}::component::server::hostname",'localhost'),
   $packages = hiera("${module_name}::component::webaccess::packages", 'zarafa-webaccess'),
 ) {
   package { $packages:
     ensure => present
   }
+
+  if downcase($serverhostname) in downcase([ $::fqdn, $::hostname ]) {
+    $zarafaserver = 'localhost'
+  }
+  else {
+    $zarafaserver = $serverhostname
+  }
+
 }
