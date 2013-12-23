d9e95da2a20eb109069764fd7b531efe3588decb
Michael Gruener <michael.gruener@chaosmoon.net>
2013-12-23 17:13:00 +0100

    Split up the ssl parameters in files and directories. This way we can set the directory parameters globally (this time also do the change for the zarafa client component).

--- manifests/component/client.pp
+++ manifests/component/client.pp
@@ -1,7 +1,8 @@
 class zarafa::component::client (
+  $sslcertdir     = hiera("${module_name}::sslcertdir",'/etc/zarafa/ssl'),
   $serverhostname = hiera("${module_name}::component::server::hostname",'localhost'),
   $packages = hiera("${module_name}::component::client::packages",['zarafa-client', 'zarafa-utils']),
-  $sslkeyfile     = hiera("${module_name}::component::client::sslkeyfile","/etc/zarafa/ssl/${::fqdn}-admin.crt"),
+  $sslkeyfile     = hiera("${module_name}::component::client::sslkeyfile","${::fqdn}-admin.crt"),
   $options        = hiera_hash("${module_name}::component::client::options",{}),
   $configfile     = hiera("${module_name}::component::client::configfile",'/etc/zarafa/admin.cfg')
 ) {
@@ -20,7 +21,7 @@ class zarafa::component::client (
                                                   value => "https://${zarafaserver}:237/zarafa"
                       },
                       'sslkey_file-client'    => { option => 'sslkey_file',
-                                                   value  => $sslkeyfile
+                                                   value  => "${sslcertdir}/${sslkeyfile}"
                       }
   }
 
