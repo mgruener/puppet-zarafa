cd92565f2c75f0ed3113665296dc5aad41921016
Michael Gruener <michael.gruener@chaosmoon.net>
2013-12-24 01:50:40 +0100

    Add the zarafauser type to add and delete users. Property manipulation is not possible at the moment because only the methods to retrieve userdata have been implemented up till now.

---
+++ lib/puppet/provider/zarafauser/db.rb
@@ -0,0 +1,206 @@
+Puppet::Type.type(:zarafauser).provide(:db) do
+  desc "Provides the ability to create zarafa users using the db user_plugin"
+
+  commands :zarafaadmin => "/bin/zarafa-admin"
+
+  has_features :zarafadbuser
+
+  def exists?
+    begin
+      zarafaadmin(['--type','user','--details',@resource[:name]])
+      return true
+    rescue => e
+      debug(e.message)
+      return false
+    end
+  end
+
+  def create
+    raise Puppet::Error, "You have to provide a full name for user #{@resource[:name]}" unless @resource[:fullname]
+    raise Puppet::Error, "You have to provide an email address for user #{@resource[:name]}" unless @resource[:email]
+    raise Puppet::Error, "You have to provide a password for user #{@resource[:name]}" unless @resource[:password]
+
+    createopts = ['-c',@resource[:name],'-p',@resource[:password],'-f',@resource[:fullname],'-e',@resource[:email]]
+    updateopts = ['-u',@resource[:name]]
+    if @resource[:admin] == :true
+      createopts << '-a' << 'yes'
+    end
+
+    if @resource[:active] == :false
+      createopts << '-n' << 'yes'
+    end
+
+    if @resource[:quotaoverride] == :true
+      updateopts << '--qo' << 'yes'
+    end
+
+    if @resource[:hardquota]
+      updateopts << '--qh' << @resource[:hardquota]
+    end
+
+    if @resource[:softquota]
+      updateopts << '--qs' << @resource[:softquota]
+    end
+
+    if @resource[:warnquota]
+      updateopts << '--qw' << @resource[:warnquota]
+    end
+
+    zarafaadmin(createopts)
+
+    if updateopts.length > 2
+      zarafaadmin(updateopts)
+    end
+
+    if @resource[:groups]
+      @resource[:groups].each do |group|
+        zarafaadmin(['-b',@resource[:name],'-i',group])
+      end
+    end
+
+    if @resource[:features]
+      @resource[:features].each do |feature|
+        zarafaadmin(['-u',@resource[:name],'--enable-feature',feature])
+      end
+    end
+  end
+
+  def destroy
+    zarafaadmin(['-d',@resource[:name]])
+  end
+
+  def features
+    getuserdetails[:features]
+  end
+
+  def features=(value)
+  end
+
+  def groups
+    getuserdetails[:groups]
+  end
+
+  def groups=(value)
+  end
+
+  def fullname
+    getuserdetails[:fullname]
+  end
+
+  def fullname=(value)
+  end
+
+  def admin
+    if getuserdetails[:admin] == 'yes'
+      :true
+    else
+      :false
+    end
+  end
+
+  def admin=(value)
+  end
+
+  def active
+    if getuserdetails[:active] == 'yes'
+      :true
+    else
+      :false
+    end
+  end
+
+  def active=(value)
+  end
+
+  def quotaoverride
+    if getuserdetails[:quotaoverride] == 'yes'
+      :true
+    else
+      :false
+    end
+  end
+
+  def quotaoverride=(value)
+  end
+
+  def hardquota
+    quota = getuserdetails[:hardquota]
+    if quota == 'unlimited'
+      0
+    else
+      quota.to_i
+    end
+  end
+
+  def hardquota=(value)
+  end
+
+  def softquota
+    quota = getuserdetails[:softquota]
+    if quota == 'unlimited'
+      0
+    else
+      quota.to_i
+    end
+  end
+
+  def softquota=(value)
+  end
+
+  def warnquota
+    quota = getuserdetails[:warnquota]
+    if quota == 'unlimited'
+      0
+    else
+      quota.to_i
+    end
+  end
+
+  def warnquota=(value)
+  end
+
+  def email
+    getuserdetails[:email]
+  end
+
+  def email=(value)
+  end
+
+private
+  def getuserdetails
+    userdetails = { :groups => []}
+    foundgroups = false
+    rawdetails = zarafaadmin(['--details',@resource[:name],'--type','user'])
+    # go through each line of the zarafa-admin --details output and extract
+    # the relevant information.
+    # When a line starts with Groups, start collection the user groups
+    # because all following lines contain the groups the user is member of
+    rawdetails.each_line do |line|
+      case line
+        when /^Username:/ then userdetails[:username] = line[/[\w ]*$/]
+        when /^Fullname:/ then userdetails[:fullname] = line[/[\w ]*$/]
+        when /^Emailaddress:/ then userdetails[:email] = line[/[\w.@ ]*$/]
+        when /^Active:/ then userdetails[:active] = line[/[\w ]*$/]
+        when /^Administrator:/ then userdetails[:admin] = line[/[\w ]*$/]
+        when /^ Quota overrides:/ then userdetails[:quotaoverride] = line[/[\w ]*$/]
+        when /^ Warning level:/ then userdetails[:warnquota] = line[/[\w ]*$/]
+        when /^ Soft level:/ then userdetails[:softquota] = line[/[\w ]*$/]
+        when /^ Hard level:/ then userdetails[:hardquota] = line[/[\w ]*$/]
+        when /^\tPR_EC_ENABLED_FEATURES\t/ then
+          tmp = line.lstrip.rstrip.split('	')[1]
+          if tmp
+            userdetails[:features] = tmp.delete(' ').split(';')
+          end
+        when /^Groups / then foundgroups = true
+        else
+          if foundgroups
+            group = line.lstrip.rstrip
+            if group != 'Everyone'
+              userdetails[:groups] << group
+            end
+          end
+      end
+    end
+    userdetails
+  end
+end
---
+++ lib/puppet/type/zarafauser.rb
@@ -0,0 +1,71 @@
+Puppet::Type.newtype(:zarafauser) do
+  @doc = "Type to create a user in a Zarafa groupware system."
+
+  feature :zarafadbuser,
+    "Create a Zarafa user in the Zarafa database."
+
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc "The username of the user."
+  end
+  
+  newproperty(:features, :array_matching => :all) do
+    desc "A list of Zarafa features the user can use, for example 'imap' or 'pop3'"
+  end
+
+  newproperty(:groups, :array_matching => :all) do
+    desc "A list of groups the user belongs to"
+  end
+
+  newproperty(:fullname) do
+    desc "The full name of the Zarafa user"
+  end
+
+  newparam(:password) do
+    desc "The password of the user. Has to be provided as cleartext."
+  end
+
+  newproperty(:admin, :boolean => true) do
+    desc "Whether the user is an admin or not"
+
+    newvalue(:true)
+    newvalue(:false)
+    
+    defaultto :false
+  end
+  
+  newproperty(:active, :boolean => true) do
+    desc "Whether the user is activ (meaning: he can log in) or not"
+
+    newvalue(:true)
+    newvalue(:false)
+
+    defaultto :true
+  end
+
+  newproperty(:quotaoverride, :booleand => true) do
+    desc "Whether to override the system wide quota settings with values specific for this user."
+
+    newvalue(:true)
+    newvalue(:false)
+
+    defaultto :false
+  end
+
+  newproperty(:hardquota) do
+    desc "The hardquota of the user. When reached, the user can no longer receive or send mails/invitations etc."
+  end
+
+  newproperty(:softquota) do
+    desc "The softquota of the user. When reached, the user can no longer send mails/invitations etc."
+  end
+
+  newproperty(:warnquota) do
+    desc "The warnquota of the user. When reached, the user will get an email informing him of the quota limits."
+  end
+
+  newproperty(:email) do
+    desc "The email address of the user"
+  end
+end
